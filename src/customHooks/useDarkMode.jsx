598068b8703d926f3739209097a65ac67fbe2385
Aashutosh <36133582+MajorSaheb@users.noreply.github.com>
2024-07-29 19:48:07 +0000

    fix: useDarkMode for SSR build

--- src/customHooks/useDarkMode.jsx
+++ src/customHooks/useDarkMode.jsx
@@ -1,15 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function useDarkMode() {
-  const [isDarkTheme, setTheme] = useState(
-    window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-  );
-  
-  window
-    .matchMedia("(prefers-color-scheme: dark)")
-    .addEventListener("change", (e) => {
-      setTheme(e.matches);
-    });
+  const [isDarkTheme, setTheme] = useState(false);
+
+  const updateTheme = (e) => {
+    setTheme(e.matches);
+  };
+
+  useEffect(() => {
+    setTheme(
+      window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+    );
+    window
+      .matchMedia("(prefers-color-scheme: dark)")
+      .addEventListener("change", updateTheme);
+    return () => {
+      window
+        .matchMedia("(prefers-color-scheme: dark)")
+        .removeEventListener("change", updateTheme);
+    };
+  }, []);
+
   return isDarkTheme;
 }
